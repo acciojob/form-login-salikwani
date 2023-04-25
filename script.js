720a3411112a91ce6ee55c3471376fa8091b3589
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 11:20:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var fname = document.getElementByName("fname").value;
+	var lname = document.getElementByName("lname").value;
+	alert(fname+" "+lname);
 }

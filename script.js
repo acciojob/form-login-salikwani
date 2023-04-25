31685ff2a69f20bcf3563c035adf4d92e6d572dc
salikwani <120441251+salikwani@users.noreply.github.com>
2023-04-25 11:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	var fname = document.querySelector("#form1 > input:nth:child(1)").value;
-	var lname = document.querySelector("#form1 > input:nth:child(2)").value;
-	alert(fname+" "+lname);
+	var fname = document.querySelector("#form1 > input:nth-child(1)").value;
+var lname = document.querySelector("#form1 > input:nth-child(3)").value;
+alert(fname+" "+lname);
 }
